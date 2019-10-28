ce7492c132af9537971df179deb3c119360880fd
Muhammad Zuhdi Zamrud <mzuhdi.zamrud@gmail.com>
2019-10-29 06:33:59 +0700

    Adding modulo and power functions and fixing some error code

--- calc.py
+++ calc.py
@@ -11,23 +11,33 @@ def mult(num1, num2):
 def div(num1, num2):
     return num1 / num2
 
+def mod(num1, num2):
+    return num1 % num2
+
+def pow(num1, num2):
+    result = 1
+    for i in range(num2):
+        result *= num1
+    return result
+
 def input_checker(item):
     try:
         checked = int(item)
         return True
     except:
-        print("Sorry your input is invalid")
+        print("\nSorry your input is invalid")
         return False
 
-print("Please select operations you want: \n 1. Adding\n 2. Substracting\n 3. Multiplying\n 4. Dividing\n")
+print("Please select operations you want: \n 1. Adding\n 2. Substracting\n 3. Multiplying\n 4. Dividing\n 5. Modulo\n 6. Powering")
 
 while True:
-    choice = input("Your choice (1,2,3,4): ")
+    choice = input("\nYour choice (1,2,3,4,5,6): ")
     if input_checker(choice):
-        if choice<1 | choice>4:
-            break
+        choice = int(choice)
+        if choice<1 or choice>6:
+            print("\nPlease input the number in range 1 and 6")
         else:
-            print("Please input the number in range 1 and 4")
+            break
 
 
 while True:
@@ -35,13 +45,19 @@ while True:
     num2 = input("Input second number: ")
 
     if input_checker(num1) & input_checker(num2):
+        num1 = int(num1)
+        num2 = int(num2)
         break
 
 if choice==1:
-    print(add(num1,num2))
+    print(num1, "+", num2, " = ", add(num1,num2))
 elif choice==2:
-    print(subs(num1,num2))
+    print(num1, "-", num2, " = ", subs(num1,num2))
 elif choice==3:
-    print(mult(num1,num2))
+    print(num1, "*", num2, " = ", mult(num1,num2))
 elif choice==4:
-    print(div(num1,num2))
+    print(num1, "/", num2, " = ", div(num1,num2))
+elif choice==5:
+    print(num1, "%", num2, " = ", mod(num1,num2))
+elif choice==6:
+    print(num1, "^", num2, " = ", pow(num1,num2))
