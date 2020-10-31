b8b932bdfcb6398aacb834647c9c19eb8854ec2c
Muhammad Zuhdi Zamrud <mzuhdi.zamrud@gmail.com>
2020-10-31 19:08:56 +0700

    Add get_user_inputs method that will ask user the first and second number to be calculated by the calculator

--- calc.py
+++ calc.py
@@ -28,20 +28,7 @@ def input_checker(item):
         print("\nSorry your input is invalid")
         return False
 
-def main():
-
-    print("Please select operations you want: \n 1. Adding\n 2. Substracting\n 3. Multiplying\n 4. Dividing\n 5. Modulo\n 6. Powering")
-
-    while True:
-        choice = input("\nYour choice (1,2,3,4,5,6): ")
-        if input_checker(choice):
-            choice = int(choice)
-            if choice<1 or choice>6:
-                print("\nPlease input the number in range 1 and 6")
-            else:
-                break
-
-
+def get_user_inputs():
     while True:
         num1 = input("Input first number: ")
         num2 = input("Input second number: ")
@@ -50,16 +37,37 @@ def main():
             num1 = int(num1)
             num2 = int(num2)
             break
+    
+    return num1, num2
 
-    if choice==1:
-        print(num1, "+", num2, " = ", add(num1,num2))
-    elif choice==2:
-        print(num1, "-", num2, " = ", subs(num1,num2))
-    elif choice==3:
-        print(num1, "*", num2, " = ", mult(num1,num2))
-    elif choice==4:
-        print(num1, "/", num2, " = ", div(num1,num2))
-    elif choice==5:
-        print(num1, "%", num2, " = ", mod(num1,num2))
-    elif choice==6:
-        print(num1, "^", num2, " = ", power(num1,num2))
+
+def main():
+
+    while True:
+        print("Please select operations you want: \n 1. Adding\n 2. Substracting\n 3. Multiplying\n 4. Dividing\n 5. Modulo\n 6. Powering")
+        choice = input("\nYour choice (1,2,3,4,5,6): ")
+        if input_checker(choice):
+            choice = int(choice)
+
+            num1, num2 = get_user_inputs()
+
+            if choice==1:
+                print(num1, "+", num2, " = ", add(num1,num2))
+            elif choice==2:
+                print(num1, "-", num2, " = ", subs(num1,num2))
+            elif choice==3:
+                print(num1, "*", num2, " = ", mult(num1,num2))
+            elif choice==4:
+                print(num1, "/", num2, " = ", div(num1,num2))
+            elif choice==5:
+                print(num1, "%", num2, " = ", mod(num1,num2))
+            elif choice==6:
+                print(num1, "^", num2, " = ", power(num1,num2))
+            else:
+                print("Choose between 1 - 6!")
+
+            print('\n')
+
+
+if __name__ == "__main__":
+    main()
